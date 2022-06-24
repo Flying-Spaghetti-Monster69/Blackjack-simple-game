77dc26fe9ac71ac9395d5b228b5d6fbf0ad7796e
Flying-Spaghetti-Monster69 <felipegarzon908@gmail.com>
2022-06-24 11:55:17 -0500

    new button and layout changes

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ let playing = true;
 let message = document.getElementById("main-message");
 let cards = document.getElementById("cards-paragraph");
 let sumedCards = document.getElementById("sum-paragraph");
+let startGameButton = document.getElementById("start-game-button")
+let newCardButton = document.getElementById("start-game-button")
 
 function checkBlackjack(sumOfCards){
-    
-    console.log(sumOfCards)
     if (sumOfCards === 21) {
     message.textContent ="you got blackjack!";
     playing = false;
@@ -14,8 +14,10 @@ function checkBlackjack(sumOfCards){
     } else {
     message.textContent = "you lost!";
     playing = false;}
-    
+}
 
+function newCard(){
+    console.log("drawing new card from the deck")
 }
 
 function getCards(){
@@ -23,12 +25,17 @@ function getCards(){
 }
 
 function startGame() {
+    startGameButton.textContent = "Restart Game";
+    cards.style.visibility = "visible";
+    sumedCards.style.visibility = "visible";
     playing = true;
     let firstCard = getCards();
+    cards.textContent = "cards: " + firstCard;
     let secondCard = getCards();
-    cards.textContent += firstCard + ", " + secondCard;
-    sumedCards.textContent += firstCard + secondCard;
+    cards.textContent += ", " + secondCard;
+    let sumOfCards = firstCard + secondCard
+    sumedCards.textContent = "sum: " + sumOfCards;
     
-    checkBlackjack(secondCard + firstCard);
+    checkBlackjack(sumOfCards);
     
 }
